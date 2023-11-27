e60f2a3abdf6508a5a84d7b7ca48d76aa26b7459
Reyesalv20 <124184473+Reyesalv20@users.noreply.github.com>
2023-11-27 07:10:10 -0600

    cambios macizones

--- winn_sceen.gd
+++ winn_sceen.gd
@@ -1,7 +1,6 @@
 extends CanvasLayer
+@onready var waitingscreenmusic = $AudioStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
