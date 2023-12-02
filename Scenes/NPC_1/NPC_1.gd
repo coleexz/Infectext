f91fa559e3e4040e6a62ab2acbeabe01af8b62da
Reyesalv20 <124184473+Reyesalv20@users.noreply.github.com>
2023-12-02 14:06:37 -0600

    cc

--- Scenes/NPC_1/NPC_1.gd
+++ Scenes/NPC_1/NPC_1.gd
@@ -1,7 +1,29 @@
 extends CharacterBody2D
 
+
 @onready var anim = $AnimatedSprite2D
-var player : Player
+var player_inrange=false
+@export var player : Player
+var dialogue_shown = false
 
 func _ready():
 	anim.play("idle")
+func _physics_process(delta):
+	if player_inrange and not dialogue_shown:
+		if Input.is_action_just_pressed("ui_accept"):
+			#player.cambiaricononpc("KNIGHT")
+			DialogueManager.show_example_dialogue_balloon(load("res://rey.dialogue"),"InfectedKing")
+			dialogue_shown=true
+			return
+	
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("Player"):
+		player_inrange = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.has_method("Player"):
+		player_inrange = false
